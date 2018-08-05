4b9407a7a2ccedd47813f5cd735b28c037912c38
Tom Biskupic <tom.biskupic@gmail.com>
2018-08-05 20:47:17 +1000

    Changed sorting of results so it is now fixed but it sorts on something sensible.

--- src/main/java/com/runcible/abbot/repository/RaceResultRepository.java
+++ src/main/java/com/runcible/abbot/repository/RaceResultRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Repository;
 
 import com.runcible.abbot.model.RaceResult;
@@ -15,10 +16,10 @@ import com.runcible.abbot.model.RaceResult;
 @Repository
 public interface RaceResultRepository extends PagingAndSortingRepository<RaceResult, Integer>
 {
-	@Query("select r from RaceResult r where r.raceId = :raceid")
+	@Query("select r from RaceResult r where r.raceId = :raceid order by r.finishTime NULLS LAST, r.status")
 	public Page<RaceResult> findRaceResults(@Param("raceid") Integer raceid,Pageable p);
 	
-	@Query("select r from RaceResult r where r.raceId = :raceid")
+	@Query("select r from RaceResult r where r.raceId = :raceid order by r.finishTime NULLS LAST, r.status")
 	public List<RaceResult> findRaceResults(@Param("raceid") Integer raceid);
 	
 	@Query("select count(rs) from RaceResult rs where rs.boat.id = :boatId and rs.handicapPlace = 1 and rs.raceId in ( select r.id from Race r where r.raceDate < :thisDate and r.fleet.id = :fleetId and r.raceSeriesId = :raceSeriesId)")
