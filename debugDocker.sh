7521ea48a99dde1bef258378ade14e0b51489ec8
Tom Biskupic <tom.biskupic@gmail.com>
2019-02-06 18:44:44 +1100

    Dependency changes

--- debugDocker.sh
+++ debugDocker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-docker run -p8080:8080 -p8000:8000 --rm -v AbbotDB:/opt/Abbot --entrypoint "catalina.sh" tombi/abbot jpda run
+docker run -p8080:8080 -p8000:8000 --rm -v AbbotDB:/opt/Abbot --link abbot-mysql --entrypoint "catalina.sh" tombi/abbot jpda run
