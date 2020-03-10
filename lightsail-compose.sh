fc6603952778febe904b4619472451a54a6089db
tom.biskupic@gmail.com <tom.biskupic@gmail.com>
2020-03-10 20:57:01 +1100

    Fixed repo URL

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -21,11 +21,12 @@ ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 # 
 #	Copy the dockerfile into /srv/docker 
 #
+mkdir /srv
 mkdir /srv/docker
-curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/tombi/abbot/master/docker-compose.yml
+curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/tom-biskupic/Abbot/master/docker-compose.yml
 
 #
 # Copy in the script required to make docker compose a daemon
 #
-curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/tombi/abbot/master/docker-compose-app.service
+curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/tom-biskupic/Abbot/master/docker-compose-app.service
 systemctl enable docker-compose-app
--- lightsail-startup.sh
+++ lightsail-startup.sh
@@ -1,7 +1,7 @@
 #
 # Use a ubuntu OS only image
 #
-curl -o lightsail-compose.sh https://raw.githubusercontent.com/tombi/abbot/master/lightsail-compose.sh
+curl -o lightsail-compose.sh https://raw.githubusercontent.com/tom-biskupic/Abbot/master/lightsail-compose.sh
 
 chmod +x ./lightsail-compose.sh
 
