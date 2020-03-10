b8021195953176a7030d81e426aed5c4675e8209
tom.biskupic@gmail.com <tom.biskupic@gmail.com>
2020-03-10 22:11:42 +1100

    Upped docker-compose version

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -7,7 +7,7 @@ curl -sSL https://get.docker.com | sh
 usermod -aG docker ubuntu
 
 # install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # 
