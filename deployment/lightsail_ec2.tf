ecd0847924247254a6f3100fba43049f174708ea
Tom Biskupic <tom.biskupic@gmail.com>
2026-01-05 20:13:59 +1100

    More work on lets encrypt

--- deployment/lightsail_ec2.tf
+++ deployment/lightsail_ec2.tf
@@ -6,7 +6,7 @@ resource "aws_lightsail_instance" "abbot_instance" {
   availability_zone = var.availability_zone
   blueprint_id      = var.blueprint_id
   bundle_id         = var.bundle_id
-  user_data         = "export BRANCH=${var.branch} && curl https://raw.githubusercontent.com/tom-biskupic/Abbot/refs/heads/${var.branch}/lightsail-compose.sh | bash"
+  user_data         = "export BRANCH=${var.branch} && export DOMAIN_NAME=${var.hostname}.${var.domain_name} && curl https://raw.githubusercontent.com/tom-biskupic/Abbot/refs/heads/${var.branch}/lightsail-compose.sh | bash"
   key_pair_name     = aws_lightsail_key_pair.aws_light_sail_abbot_key_pair.id
 }
 
@@ -23,6 +23,15 @@ resource "aws_lightsail_instance_public_ports" "abbot_instance" {
     protocol  = "tcp"
     from_port = 443
     to_port   = 443
+  }
+
+  #
+  # Needed for certbot to do http-01 challenge
+  #
+  port_info {
+    protocol  = "tcp"
+    from_port = 80
+    to_port   = 80
   }
 }
 
--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-dnf install -y docker libxcrypt-compat git
+dnf install -y docker libxcrypt-compat certbot cronie
+systemctl start crond
+systemctl enable crond
 
 # make it so you don't need to sudo to run docker commands
 usermod -aG docker ec2-user
@@ -17,10 +19,28 @@ chmod +x /usr/libexec/docker/cli-plugins/docker-compose
 mkdir -p /opt/abbot
 curl -o /opt/abbot/docker-compose.yml https://raw.githubusercontent.com/tom-biskupic/Abbot/refs/heads/${BRANCH}/docker-compose.yml
 
-#
-#	Add lets encrypt
-#
-git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
+certbot certonly --non-interactive --agree-tos -d $DOMAIN_NAME -m $EMAIL --standalone
+mkdir -p /opt/abbot/certs
+
+cat << EOF > /opt/abbot/make-p12.sh
+#!/bin/bash
+DOMAIN_NAME=\$1
+openssl pkcs12 -export \\
+  -out /opt/abbot/certs/keystore.p12 \\
+  -inkey /etc/letsencrypt/live/\$DOMAIN_NAME/privkey.pem \\
+  -in /etc/letsencrypt/live/\$DOMAIN_NAME/fullchain.pem \\
+  -passout  pass:changeit \\
+  -name "abbotracemanager"
+EOF
+chmod +x /opt/abbot/make-p12.sh
+/opt/abbot/make-p12.sh $DOMAIN_NAME
+
+cat <<EOF > /opt/abbot/renew-certs.sh
+#!/bin/bash
+certbot renew --pre-hook "systemctl stop docker-compose-app" --post-hook "/opt/abbot/make-p12.sh $DOMAIN_NAME && systemctl start docker-compose-app" >> /var/log/le-renew.log
+EOF
+chmod +x /opt/abbot/renew-certs.sh
+(crontab -l 2>/dev/null; echo "30 2 * * 1 /opt/abbot/renew-certs.sh") | crontab -
 
 #
 # Copy in the script required to make docker compose a daemon
